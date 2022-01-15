1687df7e8992bed225f64601ba3880fc998313cb
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2022-01-15 18:27:30 +0000

    finish reader

--- src/packets/reader.rs
+++ src/packets/reader.rs
@@ -18,10 +18,72 @@ impl Reader {
         self.offset += amount;
     }
 
+    /// Reads a primitive type `T` from the buffer.
     pub fn read_int<T: Readable>(&mut self) -> T {
         let value = T::from_le_bytes(&self.buf[self.offset..self.offset + T::SIZE]);
         self.incr_offset(T::SIZE);
         return value;
+    }
+
+    // Maybe this should be part of read_int. Would be easily doable.
+    pub fn read_f32(&mut self) -> f32 {
+        let val = f32::from_le_bytes(self.buf[self.offset..self.offset + 4]
+            .try_into()
+            .expect("Should never happen.")
+        );
+        self.incr_offset(4);
+        val
+    }
+
+    /// Reads a 128bit unsigned LEB integer from the buffer.
+    pub fn read_uleb128(&mut self) -> u32 {
+        let mut shift = 0_u32;
+        let mut val = 0_u32;
+
+        loop {
+            let cur_byte = self.read_int::<u8>() as u32;
+            val |= (cur_byte & 0b01111111) << shift;
+
+            if cur_byte & 0b10000000 == 0 {
+                break;
+            }
+
+            shift += 7;
+        }
+        val
+    }
+
+    /// Reads an osu style string from the buffer.
+    pub fn read_str(&mut self) -> String {
+        // Check exists byte.
+        if self.read_int::<u8>() != 0x0b {
+            return String::new();
+        }
+
+        // read string len.
+        let len = self.read_uleb128() as usize;
+        let string = String::from_utf8(self.buf[self.offset..self.offset + len].into())
+            .unwrap_or(String::new());
+        
+        self.incr_offset(len as usize);
+
+        string
+    }
+
+    /// Reads a list of i32s precremented by an u16 specifying length.
+    pub fn read_i32_list(&mut self) -> Vec<i32> {
+        let len: u16 = self.read_int();
+
+        if len == 0 {
+            return Vec::new();
+        }
+
+        let mut l = Vec::with_capacity(len as usize);
+        for _ in 0..(len as usize) {
+            l.push(self.read_int());
+        }
+
+        l
     }
 }
 
